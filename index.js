f5f1aeac77800ee06978658e7ca51f443c212fda
Guglielmo Faglioni <guglielmo@gorillatech.io>
2024-06-18 10:17:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import * as dotenv from "dotenv";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
-import { JsonOutputParser } from "@langchain/core/output_parsers";
-import { CallbackHandler } from "langfuse-langchain";
+import {StringOutputParser} from "@langchain/core/output_parsers";
+import {CallbackHandler, Langfuse} from "langfuse-langchain";
 
 dotenv.config();
 
@@ -10,11 +10,22 @@ const prompt = PromptTemplate.fromTemplate(`Tell me a joke about {param}`);
 
 const llm = new ChatOpenAI({modelName: "gpt-4-turbo", verbose: true});
 
-const parser = new JsonOutputParser();
+const parser = new StringOutputParser();
 const chain = prompt.pipe(llm).pipe(parser).withConfig({ runName: "getRelatedQuestions" });
 
 chain.name = "getRelatedQuestions";
 
-await chain.invoke({ param: "a black cat"}, { callbacks: [new CallbackHandler({flushAt: 1})] });
+const langfuse = new Langfuse();
+
+const trace = langfuse.trace({
+    name: "getRelatedQuestions",
+});
+
+const langfuseHandler = new CallbackHandler({
+    root: trace,
+    flushAt: 1
+});
+
+await chain.invoke({ param: "a black cat"}, { callbacks: [langfuseHandler] });
 
 
